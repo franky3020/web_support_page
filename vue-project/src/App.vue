c267bd037eff401805d9972f3407f8ab9dbc61f1
franky3020 <210238hwsh@gmail.com>
2024-03-15 15:35:18 +0800

    新增初步的聯絡頁面

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,34 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <el-form :model="form" label-width="auto" style="max-width: 600px">
+    <el-form-item label="Your Eamil">
+      <el-input v-model="form.email" />
+    </el-form-item>
+    <el-form-item label="Activity form">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Create</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<style scoped></style>
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  email: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
