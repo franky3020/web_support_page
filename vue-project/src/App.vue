56b5db3d0848f29264d1aabe63b5e7dfaaeebf17
franky3020 <210238hwsh@gmail.com>
2024-03-17 00:29:24 +0800

    feat: 支援頁面前端

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <el-form :model="form" label-width="auto" style="max-width: 600px">
-    <el-form-item label="Your Eamil">
-      <el-input v-model="form.email" />
-    </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="form.desc" type="textarea" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">Create</el-button>
-      <el-button>Cancel</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="flex justify-center items-center">
+        <div class="flex justify-center ">
+          <h1 class="text-2xl ">Support Page</h1>
+        </div>
+      </el-header>
+      <el-main>
+        <div class="flex justify-center">
+          <el-form class="w-9/12" :model="form" label-width="auto" style="max-width: 800px">
+            <el-form-item class="my-4" label="Your Eamil">
+              <el-input v-model="form.email" />
+            </el-form-item>
+            <el-form-item class="my-7" label="Message">
+              <el-input v-model="form.desc" type="textarea" :rows="5"/>
+            </el-form-item>
+            <div class="flex justify-center my-20">
+              <el-button type="primary" @click="onSubmit">Send Message</el-button>
+            </div>
+          </el-form>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script lang="ts" setup>
